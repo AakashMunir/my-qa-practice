d276adc1b6f9144f6166c8de7724dfe309052bf6
AakashMunir <aakash.munir@gourmetlahore.com>
2026-04-02 18:49:18 +0500

    Add the New Project of Playwright

---
+++ CICD-TestCases/TestCases/FirstTest.py
@@ -0,0 +1,25 @@
+import re
+import time
+import os
+from datetime import datetime
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://10.166.1.88/#login")
+    page.get_by_role("textbox", name="Email address or Username").click()
+    page.get_by_role("textbox", name="Email address or Username").fill("013076")
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill("@qwerty#123")
+    page.get_by_role("button", name="Login with LDAP").click()
+    time.sleep(8)
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
