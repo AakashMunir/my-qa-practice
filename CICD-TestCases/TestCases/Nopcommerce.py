7552716b1150eb89f12e283c2af56efcb0120bdd
AakashMunir <38459354+AakashMunir@users.noreply.github.com>
2026-04-03 16:38:59 +0500

    Nopcommerce headless

--- CICD-TestCases/TestCases/Nopcommerce.py
+++ CICD-TestCases/TestCases/Nopcommerce.py
@@ -6,7 +6,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://demo.nopcommerce.com/login?returnUrl=%2F")
