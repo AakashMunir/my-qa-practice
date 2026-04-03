c5c8ba7fffe3038b642f1d8aa987ec25901f50aa
AakashMunir <38459354+AakashMunir@users.noreply.github.com>
2026-04-03 17:03:35 +0500

    Nop update in pytest formate

--- CICD-TestCases/TestCases/Nopcommerce.py
+++ CICD-TestCases/TestCases/Nopcommerce.py
@@ -1,27 +1,13 @@
-import re
+# Nopcommerce.py - Pytest style mein convert karein
+import pytest
 import time
-import os
-from datetime import datetime
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Page
 
-
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context()
-    page = context.new_page()
+def test_nopcommerce_login(page: Page):
     page.goto("https://demo.nopcommerce.com/login?returnUrl=%2F")
     page.get_by_role("textbox", name="Email:").fill("mailk@mailk.com")
     page.get_by_role("textbox", name="Email:").press("Tab")
     page.get_by_role("textbox", name="Password:").fill("Malik0313")
     page.get_by_role("button", name="Log in").click()
     time.sleep(8)
-    page.get_by_role("heading", name="demo.nopcommerce.com").click()
-    time.sleep(8)
-    # ---------------------
-    # Add ne line
-    context.close()
-    browser.close()
 
-
-with sync_playwright() as playwright:
-    run(playwright)
