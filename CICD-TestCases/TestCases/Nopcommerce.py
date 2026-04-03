10831b63a680157f5b2fa0b7aae0ae7572d49d6a
AakashMunir <38459354+AakashMunir@users.noreply.github.com>
2026-04-03 18:30:21 +0500

    add time

--- CICD-TestCases/TestCases/Nopcommerce.py
+++ CICD-TestCases/TestCases/Nopcommerce.py
@@ -5,7 +5,8 @@ from playwright.sync_api import Page
 
 def test_nopcommerce_login(page: Page):
     page.goto("https://demo.nopcommerce.com/login?returnUrl=%2F")
-    time.sleep(15)
+    time.sleep(60)
+    page.wait_for_selector("//*[@id='Email']", timeout=60000)
     page.locator('//*[@id="Email"]').fill("mailk@mailk.com")
     page.locator('//*[@id="Password"]').fill("Malik0313")
     page.locator('//*[@id="main"]/div/section/div/div[2]/div[1]/div[2]/form/div[2]/button').click()
