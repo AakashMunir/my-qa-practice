79d77e5f954562989084a088c45d62575001ee97
AakashMunir <38459354+AakashMunir@users.noreply.github.com>
2026-04-06 16:44:17 +0500

    add ignore file and created allure reports

--- CICD-TestCases/TestCases/Nopcommerce.py
+++ CICD-TestCases/TestCases/Nopcommerce.py
@@ -2,14 +2,20 @@
 import pytest
 import time
 from playwright.sync_api import Page
-
+import allure
+@allure.feature("Login")
+@allure.story("Successful Login")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_nopcommerce_login(page: Page):
-    page.goto("https://gourmetfoods.pk/login")
+    with allure.step("Navigate to login page"):
+        page.goto("https://gourmetfoods.pk/login")
     time.sleep(10)
-    page.wait_for_selector("#mobile")
-    page.locator("#mobile").fill("03136326900")
-    page.locator("#password").fill("Malik0313")
-    page.get_by_role("button", name="Login").click()
+    with allure.step("Fill in credentials"):
+        page.wait_for_selector("#mobile")
+        page.locator("#mobile").fill("03136326900")
+        page.locator("#password").fill("Malik0313")
+    with allure.step("Click login button"):
+        page.get_by_role("button", name="Login").click()
     # time.sleep(8)
     # page.locator("//body/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/input[1]").fill("Aakash")
     # page.locator('//*[@id="Password"]').fill("Malik0313")
